13aecc371a019385f9a42e73ad5e203d52d37414
Yadukrishna Ks <yadukrishnaks6@gmail.com>
2026-03-07 04:44:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5188898412-66a14",
+  "appId": "1:408214785720:web:14977970f7a6e3098aae4d",
+  "apiKey": "AIzaSyA2VeQJ_sLFJ9o6y4bTR75oYan-aqehpgY",
+  "authDomain": "studio-5188898412-66a14.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "408214785720"
+};
